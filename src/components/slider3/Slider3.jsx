0be7c03561cf31ce7001f3f538283f37e3f31df9
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-06-02 12:33:13 +0530

    some done

--- src/components/slider3/Slider3.jsx
+++ src/components/slider3/Slider3.jsx
@@ -50,6 +50,14 @@ const slideContent = [
 const Slider3 = ({ onClick, className, style }) => {
   const [flipSlide, setFlipSlide] = useState(false);
   const [activeSlide, setActiveSlide] = useState(0);
+  const [goToSlide, setGoToSlide] = useState(false);
+  const handelGoToSlide = () => {
+    setGoToSlide(!goToSlide);
+    if (!goToSlide) {
+      // Reset active slide to 0 when returning to initial state
+      setActiveSlide(0);
+    }
+  };
   const handleFlipSlide = () => {
     setFlipSlide(!flipSlide);
   };
@@ -83,84 +91,125 @@ const Slider3 = ({ onClick, className, style }) => {
       console.log(activeSlide + 1);
     },
   };
+
   return (
     <>
       <div className="all-slider">
-        {activeSlide + 1 !== totalSlides && (
-          <p className="slider-pagination">
-            {activeSlide + 1}/{slideContent.length}
-          </p>
+        {!goToSlide && (
+          <>
+            <div className="play-sec">
+              <p className="play-sec-text1">Addition and Subtraction Facts within 20</p>
+              <p className="play-sec-text2">
+                Test your knowledge with the following questions, tap a card to
+                flip it and uncover the answer, good luck!
+              </p>
+              <div className="play-start-btn" onClick={() => handelGoToSlide()}>
+                <div className="play-start-icon-container">
+                  <img src="/assets/images/playIcon.svg" alt="" />
+                </div>
+                <p className="play-start-text">Let's Play</p>
+              </div>
+            </div>
+          </>
         )}
-        <Slider {...settings}>
-          {slideContent.map((items, index) => (
-            <>
-              {/* <div
+        {goToSlide && (
+          <>
+            {activeSlide + 1 !== totalSlides && (
+              <p className="slider-pagination">
+                {activeSlide + 1}/{slideContent.length}
+              </p>
+            )}
+            <Slider {...settings}>
+              {slideContent.map((items, index) => (
+                <>
+                  {/* <div
                 className={`main-slider ${flipSlide ? "flip" : ""}`}
                 key={index}
               > */}
-              {index < slideContent.length - 1 && (
-                <div
-                  className={`${
-                    index === slideContent.length - 1
-                      ? `last-slider`
-                      : `main-slider ${flipSlide ? "flip" : ""}`
-                  }`}
-                  key={index}
-                >
-                  {!flipSlide && (
-                    <div className="front-wrapper">
-                      <div className="front" onClick={() => handleFlipSlide()}>
-                        <div className="question">{items.slideHeadText}</div>
-                        <div className="bottom-text">
-                          <img src="/assets/images/tapIcon.svg" alt="icon" />
-                          <p className="tap-text">Tap to reveal the answer</p>
+                  {index < slideContent.length - 1 && (
+                    <div
+                      className={`${
+                        index === slideContent.length - 1
+                          ? `last-slider`
+                          : `main-slider ${flipSlide ? "flip" : ""}`
+                      }`}
+                      key={index}
+                    >
+                      {!flipSlide && (
+                        <div className="front-wrapper">
+                          <div
+                            className="front"
+                            onClick={() => handleFlipSlide()}
+                          >
+                            <div className="question">
+                              {items.slideHeadText}
+                            </div>
+                            <div className="bottom-text">
+                              <img
+                                src="/assets/images/tapIcon.svg"
+                                alt="icon"
+                              />
+                              <p className="tap-text">
+                                Tap to reveal the answer
+                              </p>
+                            </div>
+                          </div>
                         </div>
-                      </div>
+                      )}
+                      {flipSlide && (
+                        <div className="back-wrapper">
+                          <div
+                            className="back"
+                            onClick={() => handleFlipSlide()}
+                          >
+                            <div className="img-container">
+                              <img
+                                src={items.url}
+                                alt="icon"
+                                className="slider-img"
+                              />
+                            </div>
+                            <div className="img-text">
+                              <p>{items.imgText}</p>
+                            </div>
+                          </div>
+                        </div>
+                      )}
                     </div>
                   )}
-                  {flipSlide && (
-                    <div className="back-wrapper">
-                      <div className="back" onClick={() => handleFlipSlide()}>
-                        <div className="img-container">
+                  {index === slideContent.length - 1 && (
+                    <div className="last-slide-wrapper">
+                      <div className="last-slide">
+                        <div className="success-img-container">
                           <img
-                            src={items.url}
-                            alt="icon"
-                            className="slider-img"
+                            src="/assets/images/successTick.svg"
+                            alt=""
+                            className="success-tick"
                           />
                         </div>
-                        <div className="img-text">
-                          <p>{items.imgText}</p>
+                        <p className="success-text">
+                          Hope you learned something new!
+                        </p>
+                        <div
+                          className="play-again-btn"
+                          onClick={() => handelGoToSlide()}
+                        >
+                          <div className="play-again-icon-container">
+                            <img
+                              src="/assets/images/playAgainIcon.svg"
+                              alt=""
+                            />
+                          </div>
+                          <p className="play-again-text">Play Again</p>
                         </div>
                       </div>
                     </div>
                   )}
-                </div>
-              )}
-              {index === slideContent.length - 1 && (
-                <div className="last-slide-wrapper">
-                  <div className="last-slide">
-                    <div className="success-img-container">
-                      <img
-                        src="/assets/images/successTick.svg"
-                        alt=""
-                        className="success-tick"
-                      />
-                    </div>
-                    <p className="success-text">
-                      Hope you learned something new!
-                    </p>
-                    <div className="play-again-btn">
-                      <div className="play-again-icon-container">
-                        <img src="/assets/images/playAgainIcon.svg" alt="" />
-                      </div>
-                      <p className="play-again-text">Play Again</p>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </>
-          ))}
-        </Slider>
+                </>
+              ))}
+            </Slider>
+          </>
+        )}
       </div>
     </>
   );
